a4be85239b5e44087acff3132d76264f12af9318
Gia Khang Cao <khang2004caogia@gmail.com>
2026-05-16 13:52:48 -0400

    Add Dashboard Model

---
+++ IMA/IMA.Shared/Models/Dashboard.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IMA.Shared.Models
+{
+    public class DashboardSummary
+    {
+        public decimal totalValue { get; set; }
+        public decimal todayGain { get; set; }
+        public decimal todayGainPercent { get; set; }
+        public int totalPortfolios { get; set; }
+        public int totalPositions { get; set; }
+        public decimal monthlyGain { get; set; }
+    }
+
+    public class MonthlyPerformance
+    {
+        public string month { get; set; } = string.Empty;
+        public decimal value { get; set; }
+    }
+}
